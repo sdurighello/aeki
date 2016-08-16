2c03c1630aa76bfea4a6e4efe065dcea4dc90cd5
caisepolente <steve.durighello@gmail.com>
2016-08-16 15:55:54 +0200

    migration for AddUserToOrders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816133827) do
+ActiveRecord::Schema.define(version: 20160816134742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20160816133827) do
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
   end
 
   create_table "products", force: :cascade do |t|
@@ -65,4 +67,5 @@ ActiveRecord::Schema.define(version: 20160816133827) do
 
   add_foreign_key "inventories", "products"
   add_foreign_key "line_items", "orders"
+  add_foreign_key "orders", "users"
 end
