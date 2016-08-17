26564f227f8c2db06c656dbf8f21f410ee475106
caisepolente <steve.durighello@gmail.com>
2016-08-17 16:20:15 +0200

    re-scaffolded order and added total_price migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816154739) do
+ActiveRecord::Schema.define(version: 20160817141915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,9 +35,10 @@ ActiveRecord::Schema.define(version: 20160816154739) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.decimal  "total_price"
     t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
   end
 
