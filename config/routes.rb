af7292bfef8e8b4f99d0d07521e98f7fb160f147
thomasbrockmeier <thomas.brockmeier@gmail.com>
2016-08-17 16:33:02 +0200

    add product edit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # resources :products
   get 'products' => 'products#index'
   get 'products/new' => 'products#new', as: :new_product
+  get 'products/:id/edit' => 'products#edit', as: :edit_product
   post 'products' => 'products#create'
+  patch 'products/:id' => 'products#update'
   get 'products/:id' => 'products#show', as: :product
 
   devise_for :users
