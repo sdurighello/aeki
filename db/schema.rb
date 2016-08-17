fcfbc5ba56a3e7f3436356fc97eae2203f8eb852
Mark Nuiver <mark@vrijblikken.nl>
2016-08-17 17:12:54 +0200

    Model generated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817141915) do
+ActiveRecord::Schema.define(version: 20160817150702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,17 @@ ActiveRecord::Schema.define(version: 20160817141915) do
     t.float    "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "surname"
+    t.string   "addr_street"
+    t.string   "addr_postcode"
+    t.string   "addr_town"
+    t.string   "gender"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "roles", force: :cascade do |t|
