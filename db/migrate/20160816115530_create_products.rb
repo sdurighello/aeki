1b9ec45671bb2508a3f453d1effb83c499b44845
Ecthelion3 <matthijs.vandenberg@gmail.com>
2016-08-18 08:52:08 +0200

    seed orders and line-items

--- db/migrate/20160816115530_create_products.rb
+++ db/migrate/20160816115530_create_products.rb
@@ -4,7 +4,7 @@ class CreateProducts < ActiveRecord::Migration[5.0]
       t.string :name
       t.text :description
       t.string :image
-      t.float :price
+      t.decimal :price
 
       t.timestamps
     end
