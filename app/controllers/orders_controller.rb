71a80aa13dbbe2587ab5e2af4dbed17d2e639dad
caisepolente <steve.durighello@gmail.com>
2016-08-18 18:04:15 +0200

    commented out order_params

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -113,6 +113,6 @@ class OrdersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:total_price, :line_items)
+      # params.require(:order).permit(:total_price, :line_items)
     end
 end
