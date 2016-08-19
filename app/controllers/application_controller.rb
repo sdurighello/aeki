05a110a93b0b60c40af004834d42d30d74923abc
Ecthelion3 <matthijs.vandenberg@gmail.com>
2016-08-19 09:29:15 +0200

    add cart total quantity to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :cart_total_quantity
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
+  end
+
+  def cart_total_quantity
+    quantity = 0
+    session[:cart]['cart_items'].each do |cart_item|
+      quantity += cart_item['quantity']
+    end
+    quantity
   end
 end
