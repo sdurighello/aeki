452e0e9c6cc9a6bbf4d759f127524d248fd619fa
Mark Nuiver <mark@vrijblikken.nl>
2016-08-19 16:46:12 +0200

    Overview products

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :roles
   root 'products#index'
   # resources :products
+  get 'products/overview' => 'products#overview'
   get 'products' => 'products#index'
   get 'products/new' => 'products#new', as: :new_product
   get 'products/:id/edit' => 'products#edit', as: :edit_product
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
   patch 'products/:id' => 'products#update'
   get 'products/:id' => 'products#show', as: :product
   delete 'products/:id' => 'products#destroy'
+
 
   get 'orders' => 'orders#index'
 
