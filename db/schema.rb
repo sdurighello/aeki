cfe38b92ad3058bc048e136d2817f5c2792e8cdc
caisepolente <steve.durighello@gmail.com>
2016-08-19 08:43:48 +0200

    added quantity field to products table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817151325) do
+ActiveRecord::Schema.define(version: 20160819064033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20160817151325) do
     t.float    "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "quantity"
   end
 
   create_table "profiles", force: :cascade do |t|
