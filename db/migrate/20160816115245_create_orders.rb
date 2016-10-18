5e48e19b05032c71dbbd37dd0942f8bbd61a3fc1
caisepolente <steve.durighello@gmail.com>
2016-10-18 17:03:53 +0200

    added fields paid and payment_id to order

--- db/migrate/20160816115245_create_orders.rb
+++ db/migrate/20160816115245_create_orders.rb
@@ -1,7 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
-
+      t.integer :payment_id
+      t.boolean :paid
       t.timestamps
     end
   end
