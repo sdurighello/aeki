f456816c10712ae4896b49927dea39b17ca785d4
caisepolente <steve.durighello@gmail.com>
2016-10-18 16:11:13 +0200

    added pay_order ajax and disabled turbolinks

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,9 +17,43 @@ class OrdersController < ApplicationController
 
   def pay_order
     @order = Order.find(params[:order_id])
-    respond_to do |format|
-      format.js
+
+    # Setup Mollie
+    require 'Mollie/API/Client'
+    mollie = Mollie::API::Client.new
+    mollie.api_key = 'test_Gpt5CeadQ9BbGMbmAT8BzMVcNzvmFS'
+
+    # Create payment
+    if payment = mollie.payments.create(
+        amount: @order.total_price,
+        description: "API payment for order id #{@order.id}",
+        redirectUrl: order_pay_order_url(@order)
+      )
+      @payment = payment
+      p payment
+
+      p response.headers
+
+      # headers["X-Frame-Options"] = 'ALLOW-FROM https://www.mollie.com/'
+      #
+      # headers['Access-Control-Allow-Origin'] = '*'
+      # headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      # headers['Access-Control-Request-Method'] = '*'
+      # headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+
+      p response.headers
+
+      redirect_to payment.getPaymentUrl
+
+      respond_to do |format|
+        format.js
+        format.json { render json: @payment, status: :ok }
+      end
+
+    else
+      render :show, notice: 'Something went wrong with your payment'
     end
+
   end
 
   def history
